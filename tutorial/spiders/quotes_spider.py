c1184a90101dcbcd02272fed2c26ca7163b7388f
Dave Mackey <dave@davemackey.net>
2023-03-03 18:27:34 -0500

    First basic quotes spider

---
+++ tutorial/spiders/quotes_spider.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes" # Must be unique to project
+
+    # Returns an iterable of Requests for Spider to crawl from
+    def start_requests(self):
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    # Handles response downloaded foreach request.
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = f'quotes-{page}.html'
+        Path(filename).write_bytes(response.body)
+        self.log(f'Saved file {filename}')
