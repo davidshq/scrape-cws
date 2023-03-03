3afe89e03f657fe2e899764a2bbebb2a3606f40b
Dave Mackey <dave@davemackey.net>
2023-03-03 18:41:53 -0500

    Use CSS selectors to extract specific text

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -13,7 +13,9 @@ class QuotesSpider(scrapy.Spider):
 
     # Handles response downloaded foreach request.
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f'quotes-{page}.html'
-        Path(filename).write_bytes(response.body)
-        self.log(f'Saved file {filename}')
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
