18b9e50b93febd3b53fd4b3c80d395a81052af15
Dave Mackey <dave@davemackey.net>
2023-03-03 18:29:24 -0500

    Simplify

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -4,15 +4,12 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes" # Must be unique to project
-
-    # Returns an iterable of Requests for Spider to crawl from
-    def start_requests(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    # Can bypass start_requests() and use start_urls
+    # parse() is the default Scrapy callback
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+        'http://quotes.toscrape.com/page/2/',
+    ]
 
     # Handles response downloaded foreach request.
     def parse(self, response):
