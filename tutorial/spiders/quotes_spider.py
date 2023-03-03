9effd717fbbc6ef6b27479dcc5f6255a9982b638
Dave Mackey <dave@davemackey.net>
2023-03-03 18:48:45 -0500

    Crawl through as many pages as found

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -8,14 +8,22 @@ class QuotesSpider(scrapy.Spider):
     # parse() is the default Scrapy callback
     start_urls = [
         'http://quotes.toscrape.com/page/1/',
-        'http://quotes.toscrape.com/page/2/',
     ]
 
     # Handles response downloaded foreach request.
     def parse(self, response):
+        # Extract data using css selectors
         for quote in response.css('div.quote'):
             yield {
                 'text': quote.css('span.text::text').get(),
                 'author': quote.css('small.author::text').get(),
                 'tags': quote.css('div.tags a.tag::text').getall(),
             }
+
+        # Get the next page link, continue parsing pages
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
+
+    
